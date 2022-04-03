ff3c93bdb789aa728c7b9f2155d03f0666f1a880
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-03 21:38:57 +0600

    basic navbar created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
-        <div className='d-flex justify-content-between container'>
-            <div>DroneZone Review</div>
-            <div>
-                <p>Navlinks</p>
-            </div>
+        <div className='d-flex justify-content-center container gap-4 header-title'>
+                <Link to='/'>HOME</Link>
+                <Link to='/reviews'>REVIEWS</Link>
+                <Link to='/dashboard'>DASHBOARD</Link>
+                <Link to='/blogs'>BLOGS</Link>
         </div>
     );
 };
