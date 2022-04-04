76922cfd4919abad480d5812186e7b74a33c8969
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-04 10:16:42 +0600

    basic customer review component created

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CustomerReview = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect(()=>{
+        fetch('review.json')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    },[])
     return (
         <div>
-            <h1>Review Page</h1>
+            <h3>Total Reviews Available: {reviews.length}</h3>
+            {
+                reviews.map(review=><p>{review.name}</p>)
+            }
         </div>
     );
 };
